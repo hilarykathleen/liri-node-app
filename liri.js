5b7386f8a45263402a60d718513d2d4edbb9d68f
Hilary Lamb <hilaryklamb@gmail.com>
2019-10-19 19:09:24 -0400

    movie-this command works

--- liri.js
+++ liri.js
@@ -36,8 +36,8 @@ function concertThis() {
     axios.get("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp")
         .then(function (response) {
         // console.log(response.data[0], "response");
-        console.log(response.data[0].venue.name, "venue");
-        console.log(response.data[0].venue.city, "location");
+        console.log("venue:" + response.data[0].venue.name);
+        console.log("location:" + response.data[0].venue.city);
         var concertDate = moment(response.data[0].datetime).format("MM/DD/YYYY hh:00 A");
                     console.log(`Date and Time: ${concertDate}\n\n`);
     })
@@ -45,21 +45,32 @@ function concertThis() {
         console.log(error);
   })
 };
-// `node liri.js concert-this <artist/band name here>`
-//  * This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
-//  * Name of the venue
-//  * Venue location
-//  * Date of the Event (use moment to format this as "MM/DD/YYYY")
-//  * **Important**: There is no need to sign up for a Bands in Town `api_id` key. Use the `codingbootcamp` as your `app_id`. For example, the URL used to search for "Celine Dion" would look like the following:
-//  * `https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp`
+
 
 function spotifyThisSong() {
 console.log()
 }
 
 function movieThis() {
-console.log()
-}
+    axios.get("http://www.omdbapi.com/?t=" + userQuery + "&y=&plot=short&apikey=trilogy")
+    .then(function(response) {
+        console.log("title: " + response.data.Title);
+        console.log("year: " + response.data.Year);
+        console.log("IMBD rating: " + response.data.imdbRating);
+        console.log("rotten tomatoes rating: " + response.data.Ratings[1].Value);
+        console.log("country: " + response.data.Country);
+        console.log("language: " + response.data.Language);
+        console.log("plot: " + response.data.Plot);
+        console.log("actors: " + response.data.Actors);
+  })
+        .catch(function (error) {
+         console.log(error);
+})
+};
+
+//    * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+
+
 
 function doWhatItSays() {
 console.log()
